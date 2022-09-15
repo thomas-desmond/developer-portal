79df22bb6d5faf84a50fbe305664c37d4d47c3d2
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2022-09-15 11:03:18 -0700

    Updating middleware to point to correct URL

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,10 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const val = request.cookies.get("visitedHeadless");
-  console.log("VBEEP BOOP", val);
+  console.log("VBEEP BOOP", request.url);
   if(val && !request.url.includes('headlessHomepage')) {
     console.log("redirect");
-    return NextResponse.redirect(new URL('/personalized/headlessHomepage', 'http://localhost:3000/'))
+    return NextResponse.redirect(new URL('/personalized/headlessHomepage', request.url))
   }
   return;
 
